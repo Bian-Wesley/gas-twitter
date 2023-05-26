15f850cad18460b8c15f987c454ced793b53b9c2
Wesley Bian <wesleybian@gmail.com>
2023-05-25 17:55:01 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+function genElAppDiv(tag, text, appendTo){
+    var el = document.createElement(tag);
+    el.innerHTML = text;
+    appendTo.appendChild(el);
+}
+
+function appendTweet(tweet){
+    var allTweets = document.getElementById("tweets");
+    var tweetDiv = document.createElement("div");
+    genElAppDiv("h2", tweet.name + " said:", tweetDiv);
+    genElAppDiv("h3", tweet.tweet, tweetDiv);
+    genElAppDiv("p", "at: " + new Date(tweet.date), tweetDiv);
+    allTweets.append(tweetDiv);
+}
+
+function handleShowTweetsResp(e){
+    document.getElementById("tweets").innerHTML = "";
+    var respRaw = e.target.response;
+    var tweets = JSON.parse(respRaw);
+    for(var t = 0; t < tweets.length; t++){
+        appendTweet(tweets[t]);
+    }
+    //unset conf msg
+    document.getElementById("conf").innerHTML = "";
+}
+
+function showTweets(){
+    document.getElementById("conf").innerHTML = "Gathering tweets..."
+    var getTweets = new XMLHttpRequest();
+    getTweets.addEventListener("load", handleShowTweetsResp);
+    getTweets.open("GET", "https://script.google.com/macros/s/AKfycbzdkGVLcXM2yvKTA5-H6Z39EC8UztOFGojRRLwgC-A9Fs3rjFN4x9OLtIdoEw6kKKub/exec");
+    getTweets.send();
+}
+
+function sendTweet(){
+    //get input variables
+    var name = document.getElementById("name").value;
+    var tweet = document.getElementById("tweet").value.substring(0, 140);
+    //set confirmation message
+    document.getElementById("conf").innerHTML = "Your tweet was submitted. Reloading tweets now...";
+    //call API efficiently
+    var postTweet = new XMLHttpRequest();
+    postTweet.addEventListener("load", handlePostTweetResp);
+    postTweet.open("POST", "https://script.google.com/macros/s/AKfycbzdkGVLcXM2yvKTA5-H6Z39EC8UztOFGojRRLwgC-A9Fs3rjFN4x9OLtIdoEw6kKKub/exec");
+    var payload = {
+        name: name,
+        tweet: tweet
+    };
+    postTweet.send(JSON.stringify(payload));
+}
+
+function handlePostTweetResp(e){
+    var respRaw = e.target.response;
+    var resp = JSON.parse(respRaw);
+    if(resp.success){
+        //need a delay for the get request to update
+        setTimeout(showTweets, 1000);
+        //showTweets();
+    }
+    //console.log(e.target.response);
+    
+}
